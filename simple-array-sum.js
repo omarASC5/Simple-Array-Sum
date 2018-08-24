3b56181e57f620a7555efbc04d556d5e9f07c279
omarASC5 <41080197+omarASC5@users.noreply.github.com>
2018-08-23 21:53:12 -0400

    Create simple-array-sum.js

---
+++ simple-array-sum.js
@@ -0,0 +1,15 @@
+
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    let total = 0;
+    for (let i = 0; i < ar.length; i++) {
+        total += ar[i];
+    }
+    return total;
+}
